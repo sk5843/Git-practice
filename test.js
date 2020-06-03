d6ad1c7d98c52af8c5388de59b40e4d4f3343668
Saiakhil Kovvur <sk5843@nyu.edu>
2020-06-03 23:28:58 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,29 @@
+import * as React from 'React';
+import PropTypes from 'prop-types';
+
+const Shape = ({ name, x, y, width, height }) => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        left: x,
+        top: y,
+        width: `${width}px`,
+        height: `${height}px`,
+        backgroundColor: 'rgba(0,0,0,0.2)',
+      }}
+      data-name={name}
+    ></div>
+  );
+};
+
+//Validating props
+Shape.propTypes = {
+  name: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+export { Shape };
